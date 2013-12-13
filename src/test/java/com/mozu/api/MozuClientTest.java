3318e99a89db9ec9077ce4018bb26d4aac73e0ea
Bob Hewett <bob_hewett@volusion.com>
2013-12-13 15:47:22 -0600

    Fixed tests so all test values can be configured in properties file.

--- src/test/java/com/mozu/api/MozuClientTest.java
+++ src/test/java/com/mozu/api/MozuClientTest.java
@@ -11,16 +11,17 @@ import com.mozu.api.resources.platform.developer.ApplicationVersionResource;
 import com.mozu.api.security.AuthenticationProfile;
 import com.mozu.api.security.AuthenticationScope;
 import com.mozu.api.security.SecurityTestBase;
+import com.mozu.api.utils.ConfigProperties;
 
 public class MozuClientTest extends SecurityTestBase {
     @Test
     public void MozuClientInputStreamTest () throws Exception {
         AuthenticationProfile authenticationProfile = authenticateUser(AuthenticationScope.Tenant, USER_TENANT);
-        ApiContext apiContext = new MozuApiContext(7804, null, 1, 1);
+        ApiContext apiContext = new MozuApiContext(TENANT_ID, null, 1, 1);
         
         DocumentResource resource = new DocumentResource(apiContext);
 
-        InputStream inputStream = resource.getDocumentContent(DataViewMode.Live, "files", "9c363f6e-117b-4bf5-94a1-270359d933c5", null);
+        InputStream inputStream = resource.getDocumentContent(DataViewMode.Live, "files", ConfigProperties.getStringProperty("documentTest.id"), null);
 
         assertNotNull(inputStream);
         
